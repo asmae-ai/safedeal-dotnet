6d626939d60e44e4d0a41f97dcaf689d61a0b1e5
Asmae <merabtiasmae3@gmail.com>
2026-08-05 13:37:49 +0100

    feat(scalar): add Scalar API documentation

--- SafeDeal.API/Program.cs
+++ SafeDeal.API/Program.cs
@@ -1,5 +1,6 @@
 using SafeDeal.API.Extensions;
 using SafeDeal.Infrastructure;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +10,18 @@ builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "SafeDeal API";
+        options.Theme = ScalarTheme.Purple;
+    });
+}
 
 app.UseApiMiddlewares();
 app.MapControllers();
 
 app.Run();
 
-public partial class Program { }    
+public partial class Program { }
